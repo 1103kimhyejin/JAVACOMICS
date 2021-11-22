4b91debfa9c55a2669ffc1c7f160dd569ee27a62
buuuuri <dlwngml0204@naver.com>
2021-11-22 17:59:24 +0900

    webtoon innerlist 수정

--- src/main/webapp/js/webtoonInnerList.js
+++ src/main/webapp/js/webtoonInnerList.js
@@ -2,4 +2,10 @@ $(function(){
 	$('.information').click(function(){
 		location.href='/kakaowebtoon/webtoonInnerList/webtoonExplain.jsp';
 	});
+	
+	$('.hidden').hide();
+	$('.more').click(function(){
+		$('.hidden').show();
+		$('.more').hide();
+	});
 })
