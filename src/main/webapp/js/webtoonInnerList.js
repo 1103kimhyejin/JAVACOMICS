585f1c8620974a513934e7df0a62f166ae421abb
buuuuri <dlwngml0204@naver.com>
2021-11-23 16:55:08 +0900

    innerList explain 사진, 아이콘 수정

--- src/main/webapp/js/webtoonInnerList.js
+++ src/main/webapp/js/webtoonInnerList.js
@@ -1,11 +1,18 @@
 $(function(){
-	$('.information').click(function(){
-		location.href='/kakaowebtoon/webtoonInnerList/webtoonExplain.jsp';
-	});
+	//$('.information').click(function(){
+		//location.href='/kakaowebtoon/webtoonInnerList/webtoonExplain.jsp';
+	//});
 	
 	$('.hidden').hide();
 	$('.more').click(function(){
 		$('.hidden').show();
 		$('.more').hide();
+	});
+	
+	$('.top ul:nth-child(2) li:nth-child(2)').css({"background-color":"transparent", "color":"#fff"});
+	
+	$('.top ul:nth-child(2) li').click(function(){
+		$(this).css({"background-color":"#fff", "color":"black"})
+		$('.top ul:nth-child(2) li').not($(this)).css({"background-color":"transparent", "color":"#fff"})
 	});
 })
