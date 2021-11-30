5d4256f17cf1afb8ffaf144743d783d55169fd51
angel <angel@naver.com>
2021-11-30 18:12:34 +0900

    123

---
+++ src/main/java/webtoon/service/WebtoonServiceImpl.java
@@ -0,0 +1,22 @@
+package webtoon.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import webtoon.bean.WebtoonDTO;
+import webtoon.dao.WebtoonDAO;
+
+@Service
+public class WebtoonServiceImpl implements WebtoonService {
+	
+	@Autowired
+	private WebtoonDAO webtoonDAO;
+
+	@Override
+	public List<WebtoonDTO> getHomeBodyList() {
+		
+		return webtoonDAO.getHomeBodyList();
+	}
+}
