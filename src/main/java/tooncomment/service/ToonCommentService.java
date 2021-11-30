7bc33f1b1204abdc931de02ffad8deadfcc628ed
1103kimhyejin <1103kimhyejin@gmail.com>
2021-11-30 17:04:24 +0900

    댓글 DB 저장 스프링 진행중

---
+++ src/main/java/tooncomment/service/ToonCommentService.java
@@ -0,0 +1,9 @@
+package tooncomment.service;
+
+import tooncomment.bean.ToonCommentDTO;
+
+public interface ToonCommentService {
+
+	public void toonCommentWrite(ToonCommentDTO toonCommentDTO);
+
+}
