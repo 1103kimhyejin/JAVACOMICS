a9126d15a6412b88aa887f8f2d23e8f5cd70ef24
sky <sangi4645@gmail.com>
2021-12-03 17:32:02 +0900

    로그인 로그아웃 구현, 카카오톡 로그인 하는중

--- src/main/java/toonmember/dao/ToonMemberDAO.java
+++ src/main/java/toonmember/dao/ToonMemberDAO.java
@@ -1,5 +1,7 @@
 package toonmember.dao;
 
+import java.util.Map;
+
 import toonmember.bean.ToonMemberDTO;
 
 public interface ToonMemberDAO {
@@ -7,5 +9,7 @@ public interface ToonMemberDAO {
 	public void toonMemberWrite(ToonMemberDTO toonMemberDTO);
 
 	public ToonMemberDTO checkId(String id);
+
+	public ToonMemberDTO login(Map<String, String> map);
 
 }
